91e0560592b13cf36afd4d634812faab4b39ca57
SarahSaadain <89971132+SarahSaadain@users.noreply.github.com>
2025-02-09 23:14:20 +0100

    refactor pipeline_aDNA.py to integrate MultiQC execution and adjust processing flow

--- scripts/pipeline_aDNA.py
+++ scripts/pipeline_aDNA.py
@@ -1,18 +1,19 @@
 from common_aDNA_scripts import *
 
-import generate_reads_list as generate_reads_list
-#import adapter_remove_aDNA as adapter_remove
+import adapter_remove_aDNA as adapter_remove
 import prepare_species_for_processing as prepare
 import prepare_reads_for_GD as prepare_reads_for_GD
-#import determine_poly_nt_count as determine_poly_nt_count
+import execute_multiqc as execute_multiqc
 
 def run_pipeline():
     prepare.all_species_prepare()
-    generate_reads_list.all_species_generate_reads_lists(overwrite = True)
-    prepare_reads_for_GD.all_species_prepare_reads_for_GD()
-    
-    #adapter_remove.all_species_adapter_remove()
-    #determine_poly_nt_count.all_species_poly_count()
+    execute_multiqc.all_species_multiqc_raw()
+
+    #prepare_reads_for_GD.all_species_prepare_reads_for_GD()
+    prepare_reads_for_GD.species_prepare_reads_for_GD(FOLDER_BGER)
+
+    adapter_remove.all_species_adapter_remove()
+    execute_multiqc.all_species_multiqc_trimmed()
 
 
 def main():
