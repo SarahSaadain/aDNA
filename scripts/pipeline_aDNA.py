cb21b86daf32d51f0f15228de0e5370287036e10
SarahSaadain <89971132+SarahSaadain@users.noreply.github.com>
2025-02-10 00:27:10 +0100

    feat: integrate FastQC execution for raw and trimmed data in pipeline

--- scripts/pipeline_aDNA.py
+++ scripts/pipeline_aDNA.py
@@ -3,16 +3,19 @@ from common_aDNA_scripts import *
 import adapter_remove_aDNA as adapter_remove
 import prepare_species_for_processing as prepare
 import prepare_reads_for_GD as prepare_reads_for_GD
+import execute_fastqc as execute_fastqc
 import execute_multiqc as execute_multiqc
 
 def run_pipeline():
     prepare.all_species_prepare()
+    execute_fastqc.all_species_fastqc_raw()
     execute_multiqc.all_species_multiqc_raw()
 
     #prepare_reads_for_GD.all_species_prepare_reads_for_GD()
     prepare_reads_for_GD.species_prepare_reads_for_GD(FOLDER_BGER)
 
     adapter_remove.all_species_adapter_remove()
+    execute_fastqc.all_species_fastqc_trimmed()
     execute_multiqc.all_species_multiqc_trimmed()
 
 
