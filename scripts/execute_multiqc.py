efac08ff4dc1b783e5917f28c3609d8a7ccf3083
SarahSaadain <89971132+SarahSaadain@users.noreply.github.com>
2025-02-10 00:04:01 +0100

    fix: update folder variable names and add print statements for clarity in MultiQC execution

--- scripts/execute_multiqc.py
+++ scripts/execute_multiqc.py
@@ -28,29 +28,36 @@ def multiqc_for_trimmed_data(species):
     #TODO: check if multiqc data exists    
 
     #adapter removed data
-    raw_reads_folder = get_folder_path_species_processed_adapter_removed(species)
+    trimmed_reads_folder = get_folder_path_species_processed_adapter_removed(species)
     output_folder = os.path.join(get_folder_path_species_results_qc_multiqc(species),"trimmed")
-    run_multiqc(species, raw_reads_folder, output_folder)
+    run_multiqc(species, trimmed_reads_folder, output_folder)
 
     print_success(f"Multiqc for species {species} trimmed data complete")
 
 def run_multiqc(species, reads_folder, output_folder, threads=MULTIQC_THREADS):
 
-    command = f"multiqc --threads {threads} {reads_folder[species]}/*fastq.gz -o {output_folder}"
+    command = f"multiqc --threads {threads} {reads_folder}/*fastq.gz -o {output_folder}"
     try:
         subprocess.run(command, shell=True, check=True)
     except Exception as e:
         print_error(f"Failed to run MultiQC for species {species}: {e}")
 
 def all_species_multiqc_before_after():
+
+    print("Running MultiQC for all species before and after adapter removal")
+
     for species in FOLDER_SPECIES: 
         multiqc_before_after(species)
 
 def all_species_multiqc_raw():
+    print("Running MultiQC for all species raw data")
+
     for species in FOLDER_SPECIES: 
         multiqc_for_raw_data(species)
 
 def all_species_multiqc_trimmed():
+    print("Running MultiQC for all species trimmed data")
+
     for species in FOLDER_SPECIES: 
        multiqc_for_trimmed_data(species)
         
