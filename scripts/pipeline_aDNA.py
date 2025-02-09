2468ca594fac894d5f8e4267903f5708ecb2e189
SarahSaadain <89971132+SarahSaadain@users.noreply.github.com>
2025-02-09 13:59:35 +0100

    comment out unused adapter removal and poly nucleotide count functions in pipeline script; streamline processing steps

--- scripts/pipeline_aDNA.py
+++ scripts/pipeline_aDNA.py
@@ -1,17 +1,17 @@
 from common_aDNA_scripts import *
 
 import generate_reads_list as generate_reads_list
-import adapter_remove_aDNA as adapter_remove
+#import adapter_remove_aDNA as adapter_remove
 import prepare_species_for_processing as prepare
 import prepare_reads_for_GD as prepare_reads_for_GD
-import determine_poly_nt_count as determine_poly_nt_count
+#import determine_poly_nt_count as determine_poly_nt_count
 
 def run_pipeline():
     prepare.all_species_prepare()
     generate_reads_list.all_species_generate_reads_lists(overwrite = True)
     prepare_reads_for_GD.all_species_prepare_reads_for_GD()
     
-    adapter_remove.all_species_adapter_remove()
+    #adapter_remove.all_species_adapter_remove()
     #determine_poly_nt_count.all_species_poly_count()
 
 
