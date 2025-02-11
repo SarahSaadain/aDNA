b21229bc7395dff71e452866e151ee5bc5effbd5
SarahSaadain <s.saadain@gmail.com>
2025-02-12 00:27:59 +0100

    feat: update MultiQC execution to include quality filtered and duplicates removed data

--- scripts/execute_multiqc.py
+++ scripts/execute_multiqc.py
@@ -1,12 +1,18 @@
 import os
 from common_aDNA_scripts import *
 
-def multiqc_before_after(species):
-    # before: run multiqc for raw data
+def multiqc_for_species(species):
+    # run multiqc for raw data
     multiqc_for_raw_data(species)
 
-    # after: run multiqc for trimmed data
-    multiqc_for_trimmed_data(species)
+    # run multiqc for trimmed data
+    multiqc_for_adapter_removed_data(species)
+
+    # run multiqc for quality filtered data
+    multiqc_for_quality_filtered_data(species)
+
+    # run multiqc for duplicates removed data
+    multiqc_for_duplicates_removed_data(species)
 
 def multiqc_for_raw_data(species):
     print_info(f"Running MultiQC for species {species} raw data")
@@ -25,7 +31,39 @@ def multiqc_for_raw_data(species):
 
     print_success(f"MultiQC for species {species} raw data complete")
 
-def multiqc_for_trimmed_data(species):
+def multiqc_for_quality_filtered_data(species):
+    print_info(f"Running MultiQC for species {species} quality filtered data")  
+
+    quality_filtered_fastqc_folder = get_folder_path_species_results_qc_fastqc_quality_filtered(species)
+    output_folder = get_folder_path_species_results_qc_multiqc_quality_filtered(species)
+
+    files_list = get_files_in_folder_matching_pattern(output_folder, "*.html")
+
+    if len(files_list) > 0:
+        print_warning(f"Fastqc data already exists for species {species}. Skipping.")
+        return
+
+    run_multiqc(species, quality_filtered_fastqc_folder, output_folder)
+
+    print_success(f"MultiQC for species {species} quality filtered data complete")
+
+def multiqc_for_duplicates_removed_data(species):
+    print_info(f"Running MultiQC for species {species} duplicates removed data")
+
+    duplicates_removed_fastqc_folder = get_folder_path_species_results_qc_fastqc_duplicates_removed(species)
+    output_folder = get_folder_path_species_results_qc_multiqc_duplicates_removed(species)
+
+    files_list = get_files_in_folder_matching_pattern(output_folder, "*.html")
+
+    if len(files_list) > 0:
+        print_warning(f"Fastqc data already exists for species {species}. Skipping.")
+        return
+
+    run_multiqc(species, duplicates_removed_fastqc_folder, output_folder)
+
+    print_success(f"MultiQC for species {species} duplicates removed data complete")
+
+def multiqc_for_adapter_removed_data(species):
     print_info(f"Running MultiQC for species {species} trimmed data")
 
     #adapter removed data
@@ -38,25 +76,24 @@ def multiqc_for_trimmed_data(species):
         print_warning(f"Fastqc data already exists for species {species}. Skipping.")
         return
 
-
     run_multiqc(species, trimmed_fastqc_folder, output_folder)
 
     print_success(f"Multiqc for species {species} trimmed data complete")
 
 def run_multiqc(species, fastqc_results_folder, output_folder):
 
-    command = f"multiqc {fastqc_results_folder} -o {output_folder}"
+    command = f"{PROGRAM_PATH_MULTIQC} {fastqc_results_folder} -o {output_folder}"
     try:
         subprocess.run(command, shell=True, check=True)
     except Exception as e:
         print_error(f"Failed to run MultiQC for species {species}: {e}")
 
-def all_species_multiqc_before_after():
+def all_species_multiqc():
 
     print("Running MultiQC for all species before and after adapter removal")
 
     for species in FOLDER_SPECIES: 
-        multiqc_before_after(species)
+        multiqc_for_species(species)
 
 def all_species_multiqc_raw():
     print("Running MultiQC for all species raw data")
@@ -64,15 +101,26 @@ def all_species_multiqc_raw():
     for species in FOLDER_SPECIES: 
         multiqc_for_raw_data(species)
 
-def all_species_multiqc_trimmed():
+def all_species_multiqc_adapter_removed():
     print("Running MultiQC for all species trimmed data")
 
     for species in FOLDER_SPECIES: 
-       multiqc_for_trimmed_data(species)
-        
+       multiqc_for_adapter_removed_data(species)
+
+def all_species_multiqc_quality_filtered():
+    print("Running MultiQC for all species quality filtered data")
+
+    for species in FOLDER_SPECIES: 
+        multiqc_for_quality_filtered_data(species)
+
+def all_species_multiqc_duplicates_removed():
+    print("Running MultiQC for all species duplicates removed data")
+
+    for species in FOLDER_SPECIES: 
+        multiqc_for_duplicates_removed_data(species)
 
 def main():
-    all_species_multiqc_before_after()
+    all_species_multiqc()
 
 if __name__ == "__main__":
     main()
