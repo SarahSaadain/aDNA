c8f1f837c6a7fa8c650077ade25c00d0d6d0d3af
SarahSaadain <89971132+SarahSaadain@users.noreply.github.com>
2025-02-12 21:24:11 +0100

    refactor: rename adapter removal function and enhance output reporting

--- scripts/execute_fastp_adapter_remove_and_merge.py
+++ scripts/execute_fastp_adapter_remove_and_merge.py
@@ -7,9 +7,7 @@ from common_aDNA_scripts import *
 R1_ADAPTER_SEQUENCE = "AGATCGGAAGAGCACACGTCTGAACTCCAGTCA"
 R2_ADAPTER_SEQUENCE = "AGATCGGAAGAGCGTCGTGTAGGGAAAGAGTGT"
 
-FASTP_THREADS = 10
-
-def remove_adapters(input_file_path_r1, input_file_path_r2, output_file_path_r1, adapter_sequence_r1:str = R1_ADAPTER_SEQUENCE, adapter_sequence_r2:str = R2_ADAPTER_SEQUENCE, threads:int = FASTP_THREADS):
+def remove_adapters_and_merge(input_file_path_r1, input_file_path_r2, output_file_path_r1, adapter_sequence_r1:str = R1_ADAPTER_SEQUENCE, adapter_sequence_r2:str = R2_ADAPTER_SEQUENCE, threads:int = THREADS_DEFAULT):
 
     print_info(f"Removing adapters from {input_file_path_r1} and {input_file_path_r2} ...")
 
@@ -27,6 +25,8 @@ def remove_adapters(input_file_path_r1, input_file_path_r2, output_file_path_r1,
     filepath_merge_failed_passed_r2 = output_file_path_r1.replace(FILE_ENDING_ADAPTER_REMOVED_FASTQ_GZ, "_merge_failed_passed_r2.fastq.gz")
     filepath_merge_failed_not_passed_r1 = output_file_path_r1.replace(FILE_ENDING_ADAPTER_REMOVED_FASTQ_GZ, "_merge_failed_not_passed_r1.fastq.gz")
     filepath_merge_failed_not_passed_r2 = output_file_path_r1.replace(FILE_ENDING_ADAPTER_REMOVED_FASTQ_GZ, "_merge_failed_not_passed_r2.fastq.gz")
+    filepath_merge_json_report = output_file_path_r1.replace(FILE_ENDING_ADAPTER_REMOVED_FASTQ_GZ, "_report.json")
+    filepath_merge_html_report = output_file_path_r1.replace(FILE_ENDING_ADAPTER_REMOVED_FASTQ_GZ, "_report.html")
 
     #https://github.com/OpenGene/fastp/blob/59cc2f67414e74e99d42774e227b192a3d9bb63a/README.md#all-options
     command_fastp = [
@@ -37,20 +37,25 @@ def remove_adapters(input_file_path_r1, input_file_path_r2, output_file_path_r1,
         "--out2", filepath_merge_failed_passed_r2,
         "--unpaired1", filepath_merge_failed_not_passed_r1,
         "--unpaired2", filepath_merge_failed_not_passed_r2,
+        "--merged_out", output_file_path_r1,  # Output file for R1
+        "--in1", input_file_path_r1,        # Input R1 file
+        "--in2", input_file_path_r2,         # Input R2 file
+        "--json", filepath_merge_json_report,
+        "--html", filepath_merge_html_report,
         "--merge",
         "--thread", str(threads),               # Number of threads
 
         # length filtering options
         "--length_required" , "15",             #reads shorter than length_required will be discarded, default is 15. (int [=15])
         
+        # poly
         "--trim_poly_x", "5",
+
         # quality filtering options
         "--qualified_quality_phred", "5",       #the quality value that a base is qualified. Default 15 means phred quality >=Q15 is qualified. (int [=15])
         "--unqualified_percent_limit", "40",    #how many percents of bases are allowed to be unqualified (0~100). Default 40 means 40% (int [=40])"
-        "--n_base_limit", "5",                  #if one read's number of N base is >n_base_limit, then this read/pair is discarded. Default is 5 (int [=5])
-        "--merged_out", output_file_path_r1,  # Output file for R1
-        "--in1", input_file_path_r1,        # Input R1 file
-        "--in2", input_file_path_r2         # Input R2 file
+        "--n_base_limit", "5"                  #if one read's number of N base is >n_base_limit, then this read/pair is discarded. Default is 5 (int [=5])
+       
     ]
     
     try:
@@ -82,7 +87,7 @@ def adapter_remove_and_merge_for_species(species):
 
             adapter_removed_read_file = os.path.join(get_folder_path_species_processed_adapter_removed(species), filename_merged)
             
-            remove_adapters(read_file_path[0], read_file_path[1], adapter_removed_read_file)
+            remove_adapters_and_merge(read_file_path[0], read_file_path[1], adapter_removed_read_file)
         
     except Exception as e:
         print_error(e)
