da88fc20928f5cb7270ec98813b70a3eb467b0b4
SarahSaadain <89971132+SarahSaadain@users.noreply.github.com>
2025-02-12 21:24:23 +0100

    refactor: update import paths and rename quality filtering functions in pipeline_aDNA script

--- scripts/pipeline_aDNA.py
+++ scripts/pipeline_aDNA.py
@@ -1,14 +1,15 @@
 from common_aDNA_scripts import *
 
-import adapter_remove_aDNA as adapter_remove
+import scripts.try_and_error.adapter_remove_aDNA as adapter_remove
 import prepare_species_for_processing as prepare
 import prepare_reads_for_GD as prepare_reads_for_GD
-import execute_fastqc as execute_fastqc
+import scripts.execute_fastqc as execute_fastqc
 import execute_multiqc as execute_multiqc
 import execute_fastp_adapter_remove_and_merge as execute_fastp_adapter_remove_and_merge
-import polish_fastx_quality_filter as polish_fastx_quality_filter
-import polish_sga_index_and_remove_duplicates as polish_sga_index_and_remove_duplicates
-import polish_fastx_trimmer as polish_fastx_trimmer
+import polish_fastp_quality_filter as polish_fastp_quality_filter
+import polish_fastp_deduplication as polish_fastp_deduplication
+import scripts.try_and_error.polish_sga_index_and_remove_duplicates as polish_sga_index_and_remove_duplicates
+import scripts.try_and_error.polish_fastx_trimmer as polish_fastx_trimmer
 
 def run_pipeline():
     
@@ -24,17 +25,13 @@ def run_pipeline():
     execute_fastqc.all_species_fastqc_adapter_removed()
     execute_multiqc.all_species_multiqc_adapter_removed()
 
-    polish_fastx_quality_filter.all_species_fastx_quality_filter()
+    polish_fastp_quality_filter.all_species_fastp_quality_filter()
     execute_fastqc.all_species_fastqc_quality_filtered()
     execute_multiqc.all_species_multiqc_quality_filtered()
 
-    polish_sga_index_and_remove_duplicates.all_species_polish_sga()
+    polish_fastp_deduplication.all_species_fastp_deduplication()
     execute_fastqc.all_species_fastqc_duplicates_removed()
     execute_multiqc.all_species_multiqc_duplicates_removed()
-
-    #polish_fastx_trimmer.all_species_fastx_trimmer()
-    #execute_fastqc.
-    #execute_multiqc.()
 
 def main():
     run_pipeline()
