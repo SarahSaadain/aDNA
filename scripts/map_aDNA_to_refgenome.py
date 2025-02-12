78da310e2bc5504e3e109e02d656c7e661a3064f
SarahSaadain <89971132+SarahSaadain@users.noreply.github.com>
2025-02-12 21:51:17 +0100

    feat: implement mapping of aDNA to reference genome with error handling

--- scripts/map_aDNA_to_refgenome.py
+++ scripts/map_aDNA_to_refgenome.py
@@ -2,14 +2,38 @@ import os
 import subprocess
 from common_aDNA_scripts import *
 
-def execute_map_aDNA_to_refgenome(input_file_path:str, output_file_path:str, threads:int = 20):
-    pass
+def execute_map_aDNA_to_refgenome(input_file_path:str, ref_genome_path:str, output_file_path:str, threads:int = THREADS_DEFAULT):
+    
+    print_info(f"Mapping {input_file_path} to reference genome ...")
+
+    if not os.path.exists(input_file_path):
+        raise Exception(f"Read file {input_file_path} does not exist!")
+    
+    if not os.path.exists(ref_genome_path):
+        raise Exception(f"Reference genome file {ref_genome_path} does not exist!")
+    
+    if os.path.exists(output_file_path):
+        print_info(f"Output file {output_file_path} already exists! Skipping!")
+        return
+    
+    command_bwa = f"bwa mem -t {str(threads)} {ref_genome_path} {input_file_path} > {output_file_path}"
+
+    try:
+        subprocess.run(command_bwa, shell=True, check=True)
+    except Exception as e:
+        print_error(f"Failed to run bwa for {input_file_path}: {e}")
 
 def map_aDNA_to_refgenome_for_species(species):
     print_info(f"Mapping aDNA to reference genome for species {species} ...")
 
     read_folder = get_folder_path_species_processed_duplicates_removed(species)
     list_of_read_files = get_files_in_folder_matching_pattern(read_folder, f"*{FILE_ENDING_DUPLICATES_REMOVED_FASTQ_GZ}")
+
+    if len(list_of_read_files) == 0:
+        print_warning(f"No reads found for species {species}. Skipping.")
+        return
+    
+    
 
     #output_folder = get_folder_path_species_processed_mapped_to_refgenome(species)
 
