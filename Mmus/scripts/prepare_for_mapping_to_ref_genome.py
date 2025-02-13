aef3fe0a60f7299313543df2aee7847ae4cff820
SarahSaadain <s.saadain@gmail.com>
2025-02-13 23:07:12 +0100

    feat: update mapping preparation scripts for Dmel and Mmus with combined output functionality

--- Mmus/scripts/prepare_for_mapping_to_ref_genome.py
+++ Mmus/scripts/prepare_for_mapping_to_ref_genome.py
@@ -7,31 +7,11 @@ import csv
 sys.path.insert(0, '../../scripts')
 from common_aDNA_scripts import *
 
-def generate_fastq_patterns(file_paths):
-    patterns = {}
-    
-    for path in file_paths:
-        # Extract the filename
-        filename = os.path.basename(path)
-        
-        # Extract the prefix (C1, C2, C3)
-        prefix = filename.split('_')[0]
-        
-        # Store the wildcard pattern
-        patterns[prefix] = f"{prefix}*{FILE_ENDING_DUPLICATES_REMOVED_FASTQ_GZ}"
-
-        # Extract the prefix (C1_##, C2_##, C3_##...)
-        prefix = f"{filename.split('_')[0]}_{filename.split('_')[1]}"
-
-        patterns[prefix] = f"{prefix}*{FILE_ENDING_DUPLICATES_REMOVED_FASTQ_GZ}"
-
-    return patterns
-
 def prepare():
     print_info("Preparing Bger for ref genome mapping")
 
     # find all raw fastq files
-    duplicates_removed_folder = get_folder_path_species_processed_duplicates_removed(FOLDER_BGER)
+    duplicates_removed_folder = get_folder_path_species_processed_duplicates_removed(FOLDER_MUSMUSCULUS)
     fastq_files = get_files_in_folder_matching_pattern(duplicates_removed_folder, f"*{FILE_ENDING_DUPLICATES_REMOVED_FASTQ_GZ}")
 
     # if no files are found, exit
@@ -39,24 +19,22 @@ def prepare():
         print_error("No duplicate removed fastq files found. Exiting.")
         return
     
-    for prefix, pattern in generate_fastq_patterns(fastq_files).items():
-        fastq_files_per_pattern = get_files_in_folder_matching_pattern(duplicates_removed_folder, pattern)
-        print_info(f"Found {len(fastq_files_per_pattern)} fastq files for pattern {pattern}")
+    print_info(f"Found {len(fastq_files_per_pattern)} fastq files")
 
-        output_folder = get_folder_path_species_processed_prepared_for_ref_genome(FOLDER_BGER)
-        output_file_path = os.path.join(output_folder,f"{prefix}{FILE_ENDING_FASTQ_GZ}")
+    output_folder = get_folder_path_species_processed_prepared_for_ref_genome(FOLDER_MUSMUSCULUS)
+    output_file_path = os.path.join(output_folder, f"Mmus_combined{FILE_ENDING_FASTQ_GZ}")
 
-        if os.path.exists(output_file_path):
-            print_info(f"Output file {output_file_path} already exists. Skipping.")
-            continue
+    if os.path.exists(output_file_path):
+        print_info(f"Output file {output_file_path} already exists. Skipping.")
+        return
 
-        input_pattern_path = os.path.join(duplicates_removed_folder, pattern)
+    input_pattern_path = os.path.join(duplicates_removed_folder, f"Mmus_*{FILE_ENDING_DUPLICATES_REMOVED_FASTQ_GZ}")
 
-        # call cat via subprocess
-        try:
-            print_info(f"Concatenating {len(fastq_files_per_pattern)} fastq files for pattern {pattern}")
-            cat_command = f"cat {input_pattern_path} > {output_file_path}"
-            subprocess.run(cat_command, shell=True, check=True)
-            print_success(f"Concatenation for pattern {pattern} complete")
-        except Exception as e:
-            print_error(f"Failed to concatenate fastq files for pattern {pattern}: {e}")
+    # call cat via subprocess
+    try:
+        print_info(f"Concatenating {len(fastq_files)} fastq files for pattern {input_pattern_path}")
+        cat_command = f"cat {input_pattern_path} > {output_file_path}"
+        subprocess.run(cat_command, shell=True, check=True)
+        print_success(f"Concatenation for pattern {input_pattern_path} complete")
+    except Exception as e:
+        print_error(f"Failed to concatenate fastq files for pattern {input_pattern_path}: {e}")
