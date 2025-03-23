e489ae37c068e24898f3ec2d78c9313ad3f9fb9c
SarahSaadain <89971132+SarahSaadain@users.noreply.github.com>
2025-03-23 17:43:26 +0100

    fix: update R script retrieval in plotting functions for species comparison

--- scripts/generate_plots_compare.py
+++ scripts/generate_plots_compare.py
@@ -10,7 +10,8 @@ def plot_reads_processing_result(species):
     print_info(f"Plotting reads before and after comparison for species {species_string}")
     root_folder_path = PATH_ADNA_PROJECT
     output_folder_path = get_folder_path_results_plots()
-    generate_plots.call_r_script(R_SCRIPT_PLOT_COMPARE_SPECIES_READS_BEFORE_AFTER_PROCESSING, species_string, root_folder_path, output_folder_path)
+    r_script = get_r_script(R_SCRIPT_PLOT_COMPARE_SPECIES_READS_BEFORE_AFTER_PROCESSING)
+    generate_plots.call_r_script(r_script, species_string, root_folder_path, output_folder_path)
     print_info(f"Finished reads before and after comparison for species {species_string}")
 
 def plot_depth_breadth_analysis(species: list):
@@ -18,7 +19,8 @@ def plot_depth_breadth_analysis(species: list):
     print_info(f"Plotting depth and breadth comparison for species {species_string}")
     root_folder_path = PATH_ADNA_PROJECT
     output_folder_path = get_folder_path_results_plots()
-    generate_plots.call_r_script(R_SCRIPT_PLOT_COMPARE_SPECIES_DEPTH_BREADTH, species_string, root_folder_path, output_folder_path)
+    r_script = get_r_script(R_SCRIPT_PLOT_COMPARE_SPECIES_DEPTH_BREADTH)
+    generate_plots.call_r_script(r_script, species_string, root_folder_path, output_folder_path)
     print_info(f"Finished plotting depth and breadth comparison for species {species_string}")
 
 def plot_endogenous_reads(species: list):
@@ -26,7 +28,8 @@ def plot_endogenous_reads(species: list):
     print_info(f"Plotting endogenous reads comparison for species {species_string}")
     root_folder_path = PATH_ADNA_PROJECT
     output_folder_path = get_folder_path_results_plots()
-    generate_plots.call_r_script(R_SCRIPT_PLOT_COMPARE_SPECIES_ENDOGENOUS_READS, species_string, root_folder_path, output_folder_path)
+    r_script = get_r_script(R_SCRIPT_PLOT_COMPARE_SPECIES_ENDOGENOUS_READS)
+    generate_plots.call_r_script(r_script, species_string, root_folder_path, output_folder_path)
     print_info(f"Finished plotting endogenous reads comparison for species {species_string}")
 
 def species_generate_comparison_plots(species: list):
