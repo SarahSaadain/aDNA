30dc62acc7f1e6534b2635984f240e041622e7f1
SarahSaadain <89971132+SarahSaadain@users.noreply.github.com>
2025-03-25 01:00:05 +0100

    fix: enhance pipeline script with detailed comments and improve import naming

--- scripts/pipeline_aDNA.py
+++ scripts/pipeline_aDNA.py
@@ -1,6 +1,7 @@
 from common_aDNA_scripts import *
 
-import prepare_species_for_processing as prepare
+#load individual scripts to run within the pipeline
+import prepare_species_for_processing as prepare_species_for_processing
 import execute_fastqc as execute_fastqc
 import execute_multiqc as execute_multiqc
 import execute_fastp_adapter_remove_and_merge as execute_fastp_adapter_remove_and_merge
@@ -25,47 +26,108 @@ import determine_coi_step4_create_and_map_consensus_sequence as determine_coi_st
 
 def run_pipeline():
 
-    print("Starting pipeline.")
+    print("Starting pipeline ...")
     
-    prepare.all_species_prepare()
+    ############################################################
+    # Processing of reads
+    ############################################################
+
+    # prepare species for processing
+    # this step is optional anc uses a separate prepare.py script to prepare the species
+    # for processing. This mostly includes copying the raw reads to the correct folder 
+    # and renaming them accordingly if necessary.
+    prepare_species_for_processing.all_species_prepare()
+
+    # quality control for raw reads using fastqc and multiqc
     execute_fastqc.all_species_fastqc_raw()
     execute_multiqc.all_species_multiqc_raw()
 
-    #adapter_remove.all_species_adapter_remove()
+    # adapter removal
+    # this step uses fastp to remove adapters from the raw reads
+    # it can handle single and paired end reads. paired end reads are merged
     execute_fastp_adapter_remove_and_merge.all_species_fastp_adapter_remove_and_merge()
+
+    # quality control for adapter removed reads using fastqc and multiqc
     execute_fastqc.all_species_fastqc_adapter_removed()
     execute_multiqc.all_species_multiqc_adapter_removed()
 
+    # apply quality filtering to adapter removed reads
+    # this step uses fastp to apply quality filtering to the adapter removed reads
     polish_fastp_quality_filter.all_species_fastp_quality_filter()
+
+    # quality control for quality filtered reads using fastqc and multiqc
     execute_fastqc.all_species_fastqc_quality_filtered()
     execute_multiqc.all_species_multiqc_quality_filtered()
 
+    # remove duplicates from quality filtered reads
     polish_fastp_deduplication.all_species_fastp_deduplication()
+
+    # quality control for duplicates removed reads
     execute_fastqc.all_species_fastqc_duplicates_removed()
     execute_multiqc.all_species_multiqc_duplicates_removed()
 
+    # generate quality check report (html) to easily access all qc results
     generate_quality_check_report.all_species_generate_quality_check_report()
 
+    ############################################################
+    # Mapping to reference genome
+    ############################################################
+
+    # prepare species for mapping to reference genome
+    # this step uses the processed reads and prepares them for mapping to the 
+    # reference genome by concatenating the reads and creating different fastq files.
+    # fastq files are created for the species (all reads), and per individual
     prepare_species_for_map_to_ref_genome.all_species_prepare()
     
+    # map reads to reference genome
     map_aDNA_to_refgenome.all_species_map_aDNA_to_refgenome()
 
+    # convert mapped reads from sam to bam. also sorts the bam file and indexes it
     convert_sam2bam.all_species_convert_sam_to_bam()
 
+    # quality control for mapped reads
+    # determine endogenous reads
     determine_endogenous_reads.all_species_determine_endogenous_reads()
+    
+    # determine reads processing before and after
     determine_reads_processing_result.all_species_determine_determine_reads_processing_result()
+
+    # determine read length distribution
     determine_read_length_distribution.all_species_determine_read_length_distribution()
+
+    # determine coverage depth and breadth
     determine_coverage_depth_and_breadth.all_species_determine_coverage_depth_and_breath()
 
+    ############################################################
+    # Post processing
+    ############################################################
+
+    # determine coi
     determine_coi_step1_map_to_ref_genome.all_species_map_mtdna_to_refgenome()
     determine_coi_step2_determine_regions.all_species_mtdna_get_regions()
     #determine_coi_step3_extract_coi_regions.()
     determine_coi_step4_create_and_map_consensus_sequence.all_species_create_and_map_consensus_sequence()
 
+    # extract special sequences 
     extract_special_sequences.all_species_extract_special_sequences()
 
+    ############################################################
+    # Generate plots
+    ############################################################
+
+    # generate plots for all species to visualize results
+    # these contain 
+    # 1. reads processing results before and after
+    # 2. coverage depth and breadth
+    # 3. endogenous reads
+    # 4. sequence length distribution
     generate_plots.all_species_generate_plots()
     
+    # generate comparison plots for species
+    # these contain
+    # 1. reads processing results before and after
+    # 2. coverage depth and breadth
+    # 3. endogenous reads
     generate_plots_species_compare.species_generate_comparison_plots([FOLDER_BGER, FOLDER_TRIAL_BGER])
     generate_plots_species_compare.species_generate_comparison_plots([FOLDER_TRIAL_BGER, FOLDER_TRIAL_MMUS])
 
