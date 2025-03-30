b6b255b5aaf3f2b370d42a795d26afe6599f3e47
SarahSaadain <89971132+SarahSaadain@users.noreply.github.com>
2025-03-31 00:34:18 +0200

    fix: implement mapping function for mtDNA to reference genome with input validation and output checks

--- scripts/additional_analysis/mtdna_analysis/determine_mtdna_step1_map_to_ref_genome.py
+++ scripts/additional_analysis/mtdna_analysis/determine_mtdna_step1_map_to_ref_genome.py
@@ -1,8 +1,29 @@
 import os
 from common_aDNA_scripts import *
 
-from ref_genome_processing.map_aDNA_to_refgenome import execute_bwa_map_aDNA_to_refgenome
 from ref_genome_processing.convert_ref_genome_sam2bam import execute_convert_sam_to_bam
+
+def execute_bwa_map_mtDNA_to_refgenome(input_file_path:str, ref_genome_path:str, output_file_path:str, threads:int = THREADS_DEFAULT):
+    
+    print_info(f"Mapping {input_file_path} to reference genome ...")
+
+    if not os.path.exists(input_file_path):
+        raise Exception(f"Read file {input_file_path} does not exist!")
+    
+    if not os.path.exists(ref_genome_path):
+        raise Exception(f"Reference genome file {ref_genome_path} does not exist!")
+    
+    if os.path.exists(output_file_path):
+        print_info(f"Output file {output_file_path} already exists! Skipping!")
+        return
+    
+    command_bwa = f"{PROGRAM_PATH_BWA} {PROGRAM_PATH_BWA_MEM} -M -Y -T 50 -t {str(threads)} {ref_genome_path} {input_file_path} > {output_file_path}"
+
+    try:
+        subprocess.run(command_bwa, shell=True, check=True)
+        print_success(f"Mapping {input_file_path} to reference genome complete")
+    except Exception as e:
+        print_error(f"Failed to run bwa for {input_file_path}: {e}")
 
 def map_mtdna_to_refgenome_for_species(species: str):
     print_info(f"Mapping mtdna to reference genome for species {species} ...")
@@ -37,7 +58,7 @@ def map_mtdna_to_refgenome_for_species(species: str):
             output_file_path = os.path.join(output_folder, f"{read_name}_{ref_genome_filename}{FILE_ENDING_SAM}")
 
             try:
-                execute_bwa_map_aDNA_to_refgenome(mtrna_read_file_path, ref_genome_path, output_file_path, THREADS_DEFAULT)
+                execute_bwa_map_mtDNA_to_refgenome(mtrna_read_file_path, ref_genome_path, output_file_path, THREADS_DEFAULT)
 
                 if not os.path.exists(output_file_path):
                     continue
