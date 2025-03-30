ebdb2a14701121f613295b9295606f7857b3ccb0
SarahSaadain <89971132+SarahSaadain@users.noreply.github.com>
2025-03-30 19:56:18 +0200

    feat: add check to skip plot generation if plots already exist for species comparisons

--- scripts/additional_analysis/species_comparison/analysis/generate_plots_species_compare.py
+++ scripts/additional_analysis/species_comparison/analysis/generate_plots_species_compare.py
@@ -10,6 +10,11 @@ def plot_reads_processing_result(species: list):
     print_info(f"Plotting reads before and after comparison for species {species_string}")
     root_folder_path = PATH_ADNA_PROJECT
     output_folder_path = get_folder_path_results_plots()
+
+    if len(get_files_in_folder_matching_pattern(output_folder_path, f"*{'_'.join(species)}.png")) > 0:
+        print_info(f"Plots already exist for species {species_string}. Skipping plot generation.")
+        return
+
     r_script = get_r_script(R_SCRIPT_PLOT_COMPARE_SPECIES_READS_BEFORE_AFTER_PROCESSING, scripts_base_folder)
     call_r_script(r_script, root_folder_path, species_string,  output_folder_path)
     print_info(f"Finished reads before and after comparison for species {species_string}")
@@ -22,6 +27,11 @@ def plot_depth_breadth_analysis(species: list):
     print_info(f"Plotting depth and breadth comparison for species {species_string}")
     root_folder_path = PATH_ADNA_PROJECT
     output_folder_path = get_folder_path_results_plots()
+
+    if len(get_files_in_folder_matching_pattern(output_folder_path, f"*{'_'.join(species)}.png")) > 0:
+        print_info(f"Plots already exist for species {species_string}. Skipping plot generation.")
+        return
+
     r_script = get_r_script(R_SCRIPT_PLOT_COMPARE_SPECIES_DEPTH_BREADTH, scripts_base_folder)
     call_r_script(r_script, root_folder_path, species_string, output_folder_path)
     print_info(f"Finished plotting depth and breadth comparison for species {species_string}")
@@ -34,6 +44,12 @@ def plot_endogenous_reads(species: list):
     print_info(f"Plotting endogenous reads comparison for species {species_string}")
     root_folder_path = PATH_ADNA_PROJECT
     output_folder_path = get_folder_path_results_plots()
+
+    if len(get_files_in_folder_matching_pattern(output_folder_path, f"*{'_'.join(species)}.png")) > 0:
+        print_info(f"Plots already exist for species {species_string}. Skipping plot generation.")
+        return
+
+
     r_script = get_r_script(R_SCRIPT_PLOT_COMPARE_SPECIES_ENDOGENOUS_READS, scripts_base_folder)
     call_r_script(r_script, root_folder_path, species_string, output_folder_path)
     print_info(f"Finished plotting endogenous reads comparison for species {species_string}")
