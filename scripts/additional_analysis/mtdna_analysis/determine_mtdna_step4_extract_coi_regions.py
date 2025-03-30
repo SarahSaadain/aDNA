dab9cfdd7a1e919146d39379ae98d0aabf651aaa
SarahSaadain <89971132+SarahSaadain@users.noreply.github.com>
2025-03-30 20:33:21 +0200

    feat: enhance mtDNA extraction script to validate BED file and handle FASTA input

--- scripts/additional_analysis/mtdna_analysis/determine_mtdna_step4_extract_coi_regions.py
+++ scripts/additional_analysis/mtdna_analysis/determine_mtdna_step4_extract_coi_regions.py
@@ -2,10 +2,31 @@ import os
 import subprocess
 from common_aDNA_scripts import *
 
-def execute_samtools_extract_region_by_bed_file(bam_file_path: str, mtdna_region_bed_file_path: str, output_dir: str):
+def get_region_from_bed_file(file_path):
+
+    if not os.path.exists(file_path):
+        raise Exception(f"BED file {file_path} does not exist.")
     
-    if not os.path.exists(bam_file_path):
-        raise Exception(f"BAM file {bam_file_path} does not exist.")
+    if not file_path.endswith(FILE_ENDING_BED):
+        raise Exception(f"File {file_path} is not a BED file.")
+
+    with open(file_path, 'r') as bed_file:
+        lines = [line.strip().split() for line in bed_file if line.strip()]
+    
+    if len(lines) == 0:
+        raise Exception(f"BED file {file_path} is empty.")
+    
+    if len(lines) > 1:
+        raise Exception(f"BED file {file_path} contains more than one line.")
+    
+    scaffold, start, end, *_ = lines[0]
+    position = f"{scaffold}:{start}-{end}"
+    return position
+
+def execute_samtools_extract_region_by_bed_file(fasta_file_path: str, mtdna_region_bed_file_path: str, output_dir: str):
+    
+    if not os.path.exists(fasta_file_path):
+        raise Exception(f"FASTA file {fasta_file_path} does not exist.")
     
     if not os.path.exists(mtdna_region_bed_file_path):
         raise Exception(f"mtDNA region BED file {mtdna_region_bed_file_path} does not exist.")
@@ -14,46 +35,42 @@ def execute_samtools_extract_region_by_bed_file(bam_file_path: str, mtdna_region
         raise Exception(f"Output directory {output_dir} does not exist.")
                         
     # Extract the base name of the BAM file (without path and extension)
-    base_name_bam_file = get_filename_from_path_without_extension(bam_file_path)
+    base_name_fasta_file = get_filename_from_path_without_extension(fasta_file_path)
     base_name_bed_file = get_filename_from_path_without_extension(mtdna_region_bed_file_path)
 
-    mtdna_bam = os.path.join(output_dir, f"{base_name_bam_file}_{base_name_bed_file}.bam")
+    mtdna_fasta = os.path.join(output_dir, f"{base_name_fasta_file}_{base_name_bed_file}{FILE_ENDING_FASTA}")
 
-    if os.path.exists(mtdna_bam):
-        print_info(f"mtDNA BAM file already exists: {mtdna_bam}")
+    if os.path.exists(mtdna_fasta):
+        print_info(f"mtDNA BAM file already exists: {mtdna_fasta}")
         return
 
-    command = (
-        f"{PROGRAM_PATH_SAMTOOLS} {PROGRAM_PATH_SAMTOOLS_VIEW} -b --region-file {mtdna_region_bed_file_path} {bam_file_path} > {mtdna_bam}"
-    )
+    # Get the mtDNA region from the BED file
+    try:
+        mtdna_region = get_region_from_bed_file(mtdna_region_bed_file_path)
+        print_info(f"Extracting mtDNA region: {mtdna_region}")
+    except Exception as e:
+        print_error(f"Failed to get mtDNA region from BED file {mtdna_region_bed_file_path}: {e}")
+        return
+
+    command = f"{PROGRAM_PATH_SAMTOOLS} {PROGRAM_PATH_SAMTOOLS_FAIDX} {fasta_file_path} {mtdna_region} -i  > {mtdna_fasta}"
 
      # Filter the BAM file for reads that map to the mtDNA region
     try:
-        print_info(f"Filtering {bam_file_path} for reads mapped to mtDNA region...")
+        print_info(f"Extracting mtDNA {mtdna_region} region from {fasta_file_path} to {mtdna_fasta}...")
         subprocess.run(command, shell=True, check=True)
-        print_success(f"Filtered BAM file created: {mtdna_bam}")
-        
-        # Index the filtered BAM file
-        try:
-            print_info(f"Indexing {mtdna_bam}...")
-            subprocess.run([PROGRAM_PATH_SAMTOOLS, PROGRAM_PATH_SAMTOOLS_INDEX, mtdna_bam])
-            print_success(f"Filtered and indexed BAM file created: {mtdna_bam}")
-        except Exception as e:
-            print_error(f"Failed to index BAM file {mtdna_bam}: {e}")
-            return
-
+        print_success(f"Extracted mtDNA region {mtdna_region} from {fasta_file_path} to {mtdna_fasta}")
     except Exception as e:
-        print_error(f"Failed to filter BAM file {bam_file_path} for mtDNA region: {e}")
+        print_error(f"Failed to extract mtDNA region {mtdna_region} from {fasta_file_path}: {e}")
         return
 
 def extract_mtdna_region_for_species(species: str):
     print_info(f"Extracting mtDNA regions for species: {species}")
     
-    mapped_folder = get_folder_path_species_processed_mtdna_consensus_sequences_mapped(species)
-    bam_files = get_files_in_folder_matching_pattern(mapped_folder, f"*{FILE_ENDING_SORTED_BAM}")
+    consensus_sequences_folder = get_folder_path_species_processed_mtdna_consensus_sequences(species)
+    fasta_files = get_files_in_folder_matching_pattern(consensus_sequences_folder, f"*{FILE_ENDING_FASTA}")
 
-    if len(bam_files) == 0:
-        print_warning(f"No BAM files found for species {species}. Skipping.")
+    if len(fasta_files) == 0:
+        print_warning(f"No consensus sequence files found for species {species}. Skipping.")
         return
     
     bed_folder = get_folder_path_species_results_mtdna_regions(species)
@@ -65,9 +82,9 @@ def extract_mtdna_region_for_species(species: str):
 
     result_folder = get_folder_path_species_processed_mtdna_extracted_sequence(species)
 
-    for bam_file in bam_files:
+    for fasta_file in fasta_files:
         for bed_file in bed_files:
-            execute_samtools_extract_region_by_bed_file(bam_file, bed_file, result_folder)
+            execute_samtools_extract_region_by_bed_file(fasta_file, bed_file, result_folder)
 
     print_info(f"Finished determining mtDNA regions for species {species}")
 
