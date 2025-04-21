c1f3bc9e1b8fb3bf0631fac21cbf45cf41f727f0
SarahSaadain <89971132+SarahSaadain@users.noreply.github.com>
2025-04-21 12:00:33 +0200

    feat: remove duplicate import of common_folder_functions in common_aDNA_scripts.py

--- scripts/common_aDNA_scripts.py
+++ scripts/common_aDNA_scripts.py
@@ -1,5 +1,4 @@
 from common.common_constants import *
 from common.common_config import *
 from common.common_folder_functions import *
-from common.common_folder_functions import *
 from common.common_helper_functions import *
