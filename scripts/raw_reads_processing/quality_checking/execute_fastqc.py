47ad64ed654408437f1bf278cd48d8efa1e1d44b
SarahSaadain <89971132+SarahSaadain@users.noreply.github.com>
2025-04-23 23:50:10 +0200

    feat: optimize fastqc execution by processing multiple files in a single call

--- scripts/raw_reads_processing/quality_checking/execute_fastqc.py
+++ scripts/raw_reads_processing/quality_checking/execute_fastqc.py
@@ -32,9 +32,8 @@ def fastqc_for_raw_data(species: str):
     if len(raw_reads_files) == 0:
         print_warning(f"No raw reads found for species {species}. Skipping.")
         return
-
-    for raw_read_file in raw_reads_files:
-        execute_fastqc(species, raw_read_file, output_folder)
+    
+    execute_fastqc(species, raw_reads_files, output_folder)
 
     print_success(f"fastqc for species {species} raw data complete")
 
@@ -55,9 +54,8 @@ def fastqc_for_quality_filtered_data(species: str):
     if len(quality_filtered_reads) == 0:
         print_warning(f"No quality filtered reads found for species {species}. Skipping.")
         return
-
-    for filtered_read in quality_filtered_reads:
-        execute_fastqc(species, filtered_read, output_folder)
+    
+    execute_fastqc(species, quality_filtered_reads, output_folder)
 
     print_success(f"fastqc for species {species} quality filtered data complete")
 
@@ -80,9 +78,8 @@ def fastqc_for_adapter_removed_data(species: str):
         print_warning(f"No adapter removed reads found for species {species}. Skipping.")
         return
     
-    for trimmed_read in adapter_removed_reads:
-        execute_fastqc(species, trimmed_read, output_folder)
-
+    execute_fastqc(species, adapter_removed_reads, output_folder)
+        
     print_success(f"fastqc for species {species} adapter removed data complete")
 
 def fastqc_for_duplicates_removed_data(species: str):
@@ -103,19 +100,15 @@ def fastqc_for_duplicates_removed_data(species: str):
         print_warning(f"No duplicate removed reads found for species {species}. Skipping.")
         return
     
-    for duplicates_removed_read in duplicates_removed_reads:
-        execute_fastqc(species, duplicates_removed_read, output_folder)
+    execute_fastqc(species, duplicates_removed_reads, output_folder)
 
     print_success(f"fastqc for species {species} duplicates removed data complete")
 
-def execute_fastqc(species: str, reads_file: str, output_folder: str):
+def execute_fastqc(species: str, reads_file_list: list, output_folder: str):
 
-    print_info(f"Running fastqc for reads file {reads_file}")
+    print_info(f"Running fastqc for {len(reads_file_list)} files for species {species}")
 
-    if not os.path.exists(reads_file):
-        raise Exception(f"Read file {reads_file} does not exist!")
-
-    command = f"{PROGRAM_PATH_FASTQC} -o {output_folder} -t {THREADS_DEFAULT} {reads_file}"
+    command = f"{PROGRAM_PATH_FASTQC} -o {output_folder} -t {THREADS_DEFAULT} {' '.join(reads_file_list)}"
     try:
         subprocess.run(command, shell=True, check=True)
         print_success(f"Fastqc for {reads_file} complete")
