4175a004c53277e44b7f97bd4e6e914599d4469b
SarahSaadain <s.saadain@gmail.com>
2025-05-01 13:06:57 +0200

    refactor: improve file filtering and logging in merge_all_fastq_files function

--- scripts/ref_genome_processing/prepare_species_for_map_to_ref_genome.py
+++ scripts/ref_genome_processing/prepare_species_for_map_to_ref_genome.py
@@ -29,24 +29,27 @@ def merge_all_fastq_files(species: str):
     # filter out files that contain "LB" or "EB"
     # LB = library blanks
     # EB = extraction blanks
-    fastq_files_filtered = [f for f in fastq_files if "LB" not in os.path.basename(f) and "EB" not in os.path.basename(f)]
+    fastq_files_filtered = [f for f in fastq_files if "LB" not in get_filename_from_path(f) and "EB" not in get_filename_from_path(f)]
 
     excluded_files = [f for f in fastq_files if f not in fastq_files_filtered]
     if excluded_files:
-        print_info(f"Excluded {len(excluded_files)} files: {[os.path.basename(f) for f in excluded_files]}")
+        print_debug(f"Excluded {len(excluded_files)} files: {[os.path.basename(f) for f in excluded_files]}")
 
      # if no files are found, exit
     if len(fastq_files_filtered) == 0:
         print_warning(f"No FASTQ.GZ files found for species {species} after filtering. Exiting.")
         return
+    
+    print_debug(f"Found {len(fastq_files_filtered)} relevant FASTQ.GZ files after filtering")
+    print_debug(f"Files found: {fastq_files_filtered}")
 
     # call cat via subprocess
     try:
-
         print_info(f"Concatenating {len(fastq_files_filtered)} FASTQ.GZ files (excluding 'LB' and 'EB') for pattern *{FILE_ENDING_DUPLICATES_REMOVED_FASTQ_GZ}")
        
         cat_command = f"cat {' '.join(fastq_files_filtered)} > {output_file_path}"
         print_debug(f"cat command: {cat_command}")
+        
         subprocess.run(cat_command, shell=True, check=True)
         print_success(f"Concatenation to {output_file_path} complete")
     except Exception as e:
