03ba9927621e5374a8c407b451b64c54b65277d8
SarahSaadain <89971132+SarahSaadain@users.noreply.github.com>
2025-05-03 11:49:34 +0200

    fix: add logging directory path retrieval and enhance logging configuration

--- scripts/common/common_logging.py
+++ scripts/common/common_logging.py
@@ -1,17 +1,30 @@
-
+import os
 import logging
+from datetime import datetime
+
+
+import common.common_folder_functions as common_folder_functions
 
 #####################
 # Log
 #####################
 
-# print command to terminal
-# Configure logging
+log_dir = common_folder_functions.get_folder_path_logs()
+
+# Generate a timestamped filename
+timestamp = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+log_filename = os.path.join(log_dir, f'{timestamp}_pipeline.log')
 
 logging.basicConfig(
     level=logging.INFO,
     format='[%(asctime)s] [%(levelname)s] %(message)s', 
-    datefmt='%Y-%m-%d %H:%M:%S (%Z)')
+    datefmt='%Y-%m-%d %H:%M:%S (%Z)',
+    handlers=[
+        logging.FileHandler(log_filename),
+        logging.StreamHandler()
+    ]
+)
+
 # INFO is the default level, so it will log all messages at this level and above (WARNING, ERROR, CRITICAL)
 # if you want to log DEBUG messages, change the level to logging.DEBUG
 def print_command(subprocess_command: list):  # prints subprocess commands
